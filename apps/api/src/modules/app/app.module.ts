d93becb04d87f02989fdfbf594691d60fab9398f
kamikaze0612 <buyan.bekh@gmail.com>
2026-01-02 22:59:15 +0800

    chore: bootstrap backend project

---
+++ apps/api/src/modules/app/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+
+import { TransactionsModule } from '@/modules/transactions/transactions.module';
+
+@Module({
+  imports: [TransactionsModule],
+})
+export class AppModule {}
